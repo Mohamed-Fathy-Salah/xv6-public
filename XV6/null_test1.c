6eb98b9e694c499da6b05b10bf87e4a31ccea9bb
ahmadyasser01 <47822365+ahmadyasser01@users.noreply.github.com>
2021-01-27 09:18:12 +0200

    Update null_test1.c

--- XV6/null_test1.c
+++ XV6/null_test1.c
@@ -2,21 +2,20 @@
 #include "stat.h"
 #include "user.h"
 #define NULL ((void*)0)
-
+/*
+	THIS TEST IS CREATING A POINTER MAKING IT  POINTS TO A NULL
+	THEN TRYING TO DEREFERENCE IT
+*/
 int
 main(int argc, char *argv[])
 {
   
-
-  int a;
 int *pi;     // a pointer to an integer
 
-a = 5;
-pi = &a; // pi points to a
-
-a =*pi;
 pi=NULL;
-printf(1,"%p \n" ,*pi);
+printf(1,"\t IT WILL TRAP NOW  \n");
+printf(1,"%d \n" ,*pi); //trying to dereference the pointer
+printf(1,"THE TEST IS FAILED IF THIS LINE IS PRINTED");
 
    exit();
 }
